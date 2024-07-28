cca83ba9fd848f597b71eb520dae42ce709ca427
santhishr <144308331+santhishr@users.noreply.github.com>
2024-07-28 19:48:59 +0530

    Add files via upload

---
+++ src/pages/Shared/Home.jsx
@@ -0,0 +1,13 @@
+import Globe from "@/components/magicui/globe"
+import { GlobeDemo } from "@/components/world"
+
+// import { ParticlesDemo } from "@/components/ui/ParticlesDemo"
+const Home = () => {
+    return (
+        <div className='h-full w-full flex justify-center items-center'>
+            Home page
+            <GlobeDemo/>
+        </div>
+    )
+}
+export default Home
---
+++ src/pages/Shared/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>404 Error not found</div>
+    
+  )
+}
+
+export default NotFound
